6fadaeacb556363f0f0aaf6cdec88f320e03bb89
dkelley93 <dkelley@e-c-group.com>
2016-12-12 14:21:15 -0500

    Updated UsersWithHotelingGuestAssociativeHostReport

--- UsersWithHotelingGuestAssociativeHostReport/UsersWithHotelingGuestAssociativeHostReport.java
+++ UsersWithHotelingGuestAssociativeHostReport/UsersWithHotelingGuestAssociativeHostReport.java
@@ -18,25 +18,32 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 /**
+ * Class to Print Out a report of all Users with a Hoteling Guest Associative Host.
+ *
  * Created by DavidKelley on 8/16/16.
  */
 public class UsersWithHotelingGuestAssociativeHostReport {
 
     public static void main(String[] args) {
         try {
-            BroadWorksServer bws = BroadWorksServer.getBroadWorksServer(P.getProperties().getPrimaryBroadWorksServer());
-            List<User> users = UserHelper.getAllUsersInSystem(bws);
+            BroadWorksServer broadWorksServer = BroadWorksServer.getBroadWorksServer(P.getProperties().getPrimaryBroadWorksServer());
+
+            // Get All Users in the System
+            List<User> users = UserHelper.getAllUsersInSystem(broadWorksServer);
             List<String> usersWithHotelingGuest = new ArrayList<>();
 
-            RequestHelper.requestPerObjectProducer(bws, users, User.class, UserHotelingGuest.UserHotelingGuestGetRequest.class, (user, response) -> {
+            // Iterate through Users and Check for Associative Host ID.
+            RequestHelper.requestPerObjectProducer(broadWorksServer, users, User.class, UserHotelingGuest.UserHotelingGuestGetRequest.class, (user, response) -> {
                 if (!response.isErrorResponse() && response.getHostUserId() != null) {
                     usersWithHotelingGuest.add(response.getHostUserId());
                     System.out.println("Added User: " + response.getHostUserId());
                 }
             });
 
+            // Print out totals
             System.out.println("Hoteling Guest Associated Hosts ( " + usersWithHotelingGuest.size() + " Out Of: " + users.size() + "Total Users):");
 
+            // Print Users who have a Hoteling Guest Associative Host
             if(usersWithHotelingGuest.size() > 0) {
                 for (String userId : usersWithHotelingGuest.stream().distinct().collect(Collectors.toList())) {
                     System.out.println("User ID: " + userId);
@@ -49,7 +56,7 @@ public class UsersWithHotelingGuestAssociativeHostReport {
 
             System.exit(0);
         } catch (Exception ex) {
-            System.out.println("Error while generating report - " + ex.getMessage());
+            System.err.println("Error while generating report - " + ex.getMessage());
             System.exit(1);
         }
            
