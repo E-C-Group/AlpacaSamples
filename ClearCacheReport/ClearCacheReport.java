8f6910078779f2a37a28df1653fab4d2e012dc95
alee-ecg <alee@e-c-group.com>
2017-10-06 16:42:20 -0400

    Added functionality for multiple Broadworks clusters to be used with ClearCacheReport, user provides options as follows: e: Service Provider or Enterprise ID c: Nickname of the cluster in the Alpaca Configuration file

--- ClearCacheReport/ClearCacheReport.java
+++ ClearCacheReport/ClearCacheReport.java
@@ -3,6 +3,10 @@ import co.ecg.alpaca.toolkit.generated.User;
 import co.ecg.alpaca.toolkit.helper.user.UserHelper;
 import co.ecg.alpaca.toolkit.model.BroadWorksServer;
 import co.ecg.utilities.properties.P;
+import co.ecg.utilities.properties.BroadWorksServerConfig;
+import co.ecg.utilities.properties.TimesTenConnectionConfig;
+import jline.console.ConsoleReader;
+import org.apache.commons.cli.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,21 +16,68 @@ import java.util.stream.Collectors;
  * This could be used to clear an SBC's user registration cache during migration support.
  *
  * @author Matthew Keathley
+ * modified by Ashley Lee on 10/5/2017
+ *  - accepts arguments for nickname and enterprise or service Provider ID
  */
 public class ClearCacheReport {
 
     public static void main(String[] args) {
         try {
+
+            CommandLineParser parser = new DefaultParser();
+
+            Options options = new Options();
+            options.addOption(Option.builder("-e")
+                    .desc("The Enterprise or Service Provider ID to generate the list of clear cache commands")
+                    .longOpt("EnterpriseID")
+                    .hasArg(true)
+                    .required(true)
+                    .build());
+
+            options.addOption(Option.builder("c")
+                    .desc("The BroadWorks cluster nickname")
+                    .longOpt("clusterNickName")
+                    .hasArg(true)
+                    .required(false)
+                    .build());
+
+            CommandLine cmd = null;
+
+            // Open the connection to BroadWorks
+            // If Option c was provided with a valid nickname
+            BroadWorksServer bws = null;
+
+            if (cmd.hasOption("c")) {
+                List<BroadWorksServerConfig> broadWorksServerConfigList = P.getProperties().getBroadWorksServerConfigList();
+
+                for (BroadWorksServerConfig bwsConfig: broadWorksServerConfigList) {
+                    if (cmd.getOptionValue("c").equals(bwsConfig.getNickname())) {
+                        bws = BroadWorksServer.getBroadWorksServer(bwsConfig);
+                        break;
+                    }
+                }
+            } else {
+                bws = BroadWorksServer.getBroadWorksServer(P.getProperties().getPrimaryBroadWorksServer());
+            }
+
+            if (bws == null) {
+
+            }
+
             if (args.length < 1) {
                 System.out.println("Please specify Enterprise or ServiceProvider ID.");
                 System.exit(1);
             }
 
-            // Open the connection to BroadWorks
-            BroadWorksServer bws = BroadWorksServer.getBroadWorksServer(P.getProperties().getPrimaryBroadWorksServer());
+            if (args.length < 2) {
+                System.out.println("Please specify BroadWorks Cluster nickname and Enterprise or ServiceProvider ID.");
+                System.exit(1);
+            }
+
+
 
             // Retrieve Service Provider
-            ServiceProvider serviceProvider = ServiceProvider.getPopulatedServiceProvider(bws, args[0]);
+            ServiceProvider serviceProvider = ServiceProvider.getPopulatedServiceProvider(bws, cmd.getOptionValue("e"));
 
             // Get list of Users in Service Provider
             List<User> userList = serviceProvider
